c9eb2ecfa07a6f16dc70c94e008353c9c90ea905
Najeeb Arif <NajeebArif786@outlook.com>
2021-10-20 23:31:54 +0530

    Update User.java
    Add JPA Annotations for Id and Enums

--- src/main/java/narif/mlp/rbacldap/model/User.java
+++ src/main/java/narif/mlp/rbacldap/model/User.java
@@ -2,20 +2,22 @@ package narif.mlp.rbacldap.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String firstName;
     private String lastName;
     private String emailId;
     private Integer age;
     private String phone;
+    @Enumerated(EnumType.STRING)
     private Role role;
     private String password;
     private String managerName;
